42cfb6ce3451c5613039b5f31abdae1ceae7f9b3
Shami Sharma <shamisharma01@outlook.com>
2022-10-19 16:32:04 -0400

    Added pop and peek methods

--- Stack.py
+++ Stack.py
@@ -9,3 +9,17 @@ class Stack:
         new_item.set_next_node(self.top_item)
         self.top_item = new_item
         self.size += 1
+    
+    def pop(self):
+        if self.size > 0:
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -=1
+            return item_to_remove.get_value()
+        return "The Stack is Empty!"
+    
+    def peek(self):
+        if self.size > 0:
+            return self.top_item
+        return "The Stack is Empty!"
+    
